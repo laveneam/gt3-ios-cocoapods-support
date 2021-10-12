64b825f0f3f704c922b2a76514ee7ee8014ce594
Lavinia Mihalachi <Lavinia.mihalachi@softbinator.com>
2021-10-12 19:08:00 +0300

    SPM first attempt

---
+++ Tests/gt3-ios-cocoapods-supportTests/gt3_ios_cocoapods_supportTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import gt3_ios_cocoapods_support
+
+    final class gt3_ios_cocoapods_supportTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(gt3_ios_cocoapods_support().text, "Hello, World!")
+        }
+    }
